7a71a55be626728ab73e5aa01286df74bfdabcc4
Shreedutt <schegde@cmu.edu>
2017-05-31 20:37:28 -0700

    further_cleanup

--- Single_program/transfer.c
+++ Single_program/transfer.c
@@ -15,30 +15,38 @@ typedef int buffer_type;
 #define SERVER_MODE 0
 #define CLIENT_MODE 1
 
-int MODE;
 
 #define BUFFER_PRINT
 
 //This can be set by a top level application to break the listening loop, and for a chance to change mode
-int STOP_FLAG = 0;  
+int STOP_FLAG;  
+
+int MODE;
+
+buffer_type * buffer;
+int listenfd, *connfd;
+
+
 //********************************************************************************
 
 
 
+/*
+Signal handler for switching the roles between Server and client.
+Currently configured to respond to  SIGTSTP Linux signal which gets generated from 
+(Ctrl+Z) keyboard combination while program is running.
+To respond to a signal sent by another process, send the appropriate signal to this program
+And install this handler for that signal in main();
 
-// void sendit(int fd);
-// void clienterror(int fd, char *cause, char *errnum, 
-//      char *shortmsg, char *longmsg);
-// void send_func(void *vargp);
+*/
+void sig_handler(int sig_num){
 
-// void receive_file(char *getName, int num_bytes, char *host, char *port);
-// void listen_func(void *vargp);
-// void remove_newline_ch(char *line);
-// //int Open_w(const char *pathname, int flags, mode_t mode);
-// int timeval_subtract (struct timeval *result, struct timeval *x, struct timeval *y);
+// printf("Interrupted!!\n");
 
+    STOP_FLAG = 1;      //Interrupted by an external signal.
 
-
+   
+}
 
 /*
  * 
@@ -46,29 +54,48 @@ int STOP_FLAG = 0;
  * spawning threads to handle their requests
  * 
  */
+
 int main(int argc, char **argv) {
 
-   // STOP_FLAG = 0;
-   
+    
+    Signal(SIGPIPE, SIG_IGN);
+
+    //Installing the signal handler for SIGTSSTP signal. Change to appropriate signal, if 
+    //another process will interrupt this program flow.
+
+    Signal(SIGTSTP, sig_handler); //Ctrl-Z for interruption!
+
+    poll_mode();
+       
+}
+
+
+void poll_mode(){
+
+
+
     int errno;
     char getName[MAXBUF];// copyName[MAXBUF]; 
     int num_bytes;
     
     /* simply ignore sigpipe signals, the most elegant solution */
-    Signal(SIGPIPE, SIG_IGN);
+
 
     while(1){
 
-    int MODE;
+    STOP_FLAG = 0; //Reset the flag, this is a new role start!
+
+   
     printf("Select the mode you want to operate in? Hit 1 for client mode and 0 for server mode:");
     errno = scanf("%d",&MODE);
     
 
     //Declare and initialize the buffer.
     if(MODE==SERVER_MODE){
+        //Server Mode
     char port[10];
     printf("Server mode selected!\n");
-    buffer_type * buffer = (buffer_type *)malloc(BUFFERSIZE*sizeof(buffer_type));
+    buffer = (buffer_type *)malloc(BUFFERSIZE*sizeof(buffer_type));
 
     for(int i=0;i<BUFFERSIZE;i++){
       buffer[i] = i;
@@ -78,53 +105,14 @@ int main(int argc, char **argv) {
     printf("The pointer to the buffer allocated is:%lld\n",(long long)buffer);
     printf("Enter the listening port for the server:");
     errno = scanf("%s",port);
-    listen_func(port);
-    free(buffer);
-    }
 
-    else{
-      printf("Client mode selected!\n");
-      char host[30], port[10];
-      printf("Enter the Host IP address to connect to:");
-      errno = scanf("%s",host);
-      printf("Enter the port on the host to connect to:");
-      errno = scanf("%s",port); 
-
-    while(1) {
-
-        if(STOP_FLAG)
-          break;
-        
-        printf("\nEnter The buffer pointer to receive: ");
-        errno = scanf("%s", getName); //susceptible to buffer overflow, but whatevs
-        strcat(getName, "\n");
-        printf("Enter num_bytes asked for ");
-        errno = scanf("%d", &num_bytes);
-        receive_file(getName, num_bytes, host, port);
-        
-    }
-
-    }
-    }   
- 
-     
-}
-
-void mode_selector(int mode_loc){
-
-  MODE = mode_loc;
-
-}
-
-void listen_func(void *vargp){
+    //**************************former listen func();**************
     int listenfd, *connfd;
     
     struct sockaddr_in clientaddr;
     socklen_t clientlen = sizeof(clientaddr);
 
-    char *port = vargp;
-    
-    
+      
     //bind to port
     listenfd = Open_listenfd(port);
 
@@ -135,10 +123,76 @@ void listen_func(void *vargp){
           break;
         connfd = Malloc(sizeof(int));
         *connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-        send_func(connfd);
+        int loc_connfd = *connfd;
+        Free(connfd);
+        sendit(loc_connfd);
+        Close(loc_connfd);
+        //send_func(connfd);
     }
+    //**************************
+    free(buffer);
+    }
+
     
+
+    else{
+
+        //Client mode
+      printf("Client mode selected!\n");
+      char host[30], port[10];
+      printf("Enter the Host IP address to connect to:");
+      errno = scanf("%s",host);
+      printf("Enter the port on the host to connect to:");
+      errno = scanf("%s",port); 
+
+    while(1) {
+
+               
+        printf("\nEnter The buffer pointer to receive: ");
+        errno = scanf("%s", getName); //susceptible to buffer overflow, but whatevs
+        strcat(getName, "\n");
+        printf("Enter num_bytes asked for ");
+        errno = scanf("%d", &num_bytes);
+        long long buf_pointer = atoi(getName);
+
+        receive_file(&buf_pointer, num_bytes, host, port);
+        if(STOP_FLAG)
+          break;
+        
+    }
+
+    }
+    }   
+ 
 }
+
+
+
+// void listen_func(void *vargp){
+    
+//     int listenfd, *connfd;
+    
+//     struct sockaddr_in clientaddr;
+//     socklen_t clientlen = sizeof(clientaddr);
+
+//     char *port = vargp;
+    
+    
+//     //bind to port
+//     listenfd = Open_listenfd(port);
+
+//     printf("Successfully started a listening server on  port %s, Now connect to this port from client side!\n",port);
+    
+//     while (1) {
+//         if(STOP_FLAG)
+//           break;
+//         connfd = Malloc(sizeof(int));
+//         *connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+//         send_func(connfd);
+// }
+  
+    
+// }
 
 
 /*
@@ -146,13 +200,13 @@ void listen_func(void *vargp){
  * transfer_thread - transfers requested file to client and closes when done
  * 
  */
-void send_func(void *vargp) {
-    int connfd = *((int *)vargp);
+// void send_func(void *vargp) {
+//     int connfd = *((int *)vargp);
    
-    Free(vargp);
-    sendit(connfd);
-    Close(connfd);
-}
+//     Free(vargp);
+//     sendit(connfd);
+//     Close(connfd);
+// }
 
 /*
  * sendit - sends preset file to client
@@ -168,44 +222,28 @@ void sendit(int clientfd) {
 
     //first, get the Buffer pointer we want. 
 
-    rio_readlineb(&clientRio, buf, MAXBUF);
-    remove_newline_ch(buf);
+    //rio_readlineb(&clientRio, buf, MAXBUF);
+    //remove_newline_ch(buf);
 
-    long long pointer = atoi(buf);
+    long long pointer;
+    rio_readnb(&clientRio,&pointer,sizeof(long long));
 
     printf("    Server: Client requested pointer %lld\n",pointer);
-    //  printf("Dereferencing this gives:%d\n",*((int *)pointer));
-
 
     int size_req;
     rio_readnb(&clientRio,&size_req,sizeof(int));
 
 
-    // if((filefd = Open_w(buf, O_RDONLY, DEF_MODE)) < 0) { //error opening file
-    //     strcpy(buf,"error\n");
-    //     printf("%s\n", buf);
-    //     rio_writen(clientfd, buf, strlen(buf));
-    //     return;
-    // };
+
     
     strcpy(buf,"success\n");
     rio_writen(clientfd,buf,strlen(buf));
 
-
-    // for(int i = 0;i<size_req;i++){
-    //    int_buf[i] = *(((int *)(pointer))+i);
-    // }
     buffer_type * send_buf_ptr = (buffer_type *)(pointer);
 
     rio_writen(clientfd,send_buf_ptr,size_req);
 
-    
-    // while ((length = rio_readn(filefd, buf, MAXBUF)) != 0) {
-    //     rio_writen(clientfd, buf, length);
-    // }
-
-    //finally close open file descriptor
-    //Close(filefd);
+  
     printf("    Server: Client received buffer\n");
 }
 
@@ -216,7 +254,7 @@ void remove_newline_ch(char *line) {
         line[new_line] = '\0';
 }
 
-void receive_file(char *getName, int num_bytes, char *host, char *port) {
+void receive_file(long long *ptr, int num_bytes, char *host, char *port) {
     int remotefd; //, filefd, length;
     char buf[MAXBUF];
     rio_t remoteRio;
@@ -228,7 +266,7 @@ void receive_file(char *getName, int num_bytes, char *host, char *port) {
     Rio_readinitb(&remoteRio, remotefd);
 
     //Request the buffer pointer.
-    rio_writen(remotefd, getName, strlen(getName));
+    rio_writen(remotefd, ptr,sizeof(long long));
 
     rio_writen(remotefd, &num_bytes,sizeof(int));
     
