6565ab197fb6e7e4d21c52b387164daaec76224d
Shreedutt <schegde@cmu.edu>
2017-06-02 02:26:27 -0700

    in_progress

--- Single_program/transfer.c
+++ Single_program/transfer.c
@@ -32,12 +32,31 @@ int STOP_FLAG;
 
 int CHANGE_FLAG;
 
+int DISCONTINUE_FLAG;
+
 int MODE;
 
+#define ACK 5 //random numbers assigned for ACK and NACK and STOP_MSG
+#define NACK 6
+#define STOP_MSG 7
 
 //********************************************************************************
 
 
+//***************************************************NOTES*************************************
+// 1)   START PROCEDURE:START THE SERVER FIRST, AND THEN THE CLIENT. (This order is VERY necessary)
+// 2)   STOP PROCEDURE: SEND a configured linux SIGNAL(currently configured for ctrl+z keyboard combination) 
+//                      TO THIS PROGRAM ON *CLIENT*; CLIENT THEN SENDS A MESSAGE TO SERVER TO 
+//                      stop itself, and reverse role if needed.
+// 3)   Minimum one transaction is needed to stop the respective roles and allow for role reversal :( 
+//      [This is a disadvantage of a blocking network socket, but non-blocking sockets are more tough to manage]
+// 4)   Every transaction implements a ACK/NACK/STOP_MSG handshaking between server and client for synchronization.
+// 5)   There will be timing restrictions on The control signals(STOP/CHANGE/DISCONTINUE FLAG) are given to this program.
+//      These signals can be given from a new thread in this program, to keep it asynced with the flow control of 
+//      main thread of this program.
+
+
+//*********************************************************************************************
 
 /*
 Signal handler for switching the roles between Server and client.
@@ -51,6 +70,7 @@ void sig_handler(int sig_num){
 
     STOP_FLAG = 1;      //Interrupted by an external signal.
   
+  //File descriptor cleanups and malloced cleanups! connection file desctiptors in client and server
 }
 
 /*
@@ -62,7 +82,14 @@ void sig_handler(int sig_num){
 
 int main(int argc, char **argv) {
 
-    
+    operate_mode();
+       
+}
+
+
+void operate_mode(){
+
+
     Signal(SIGPIPE, SIG_IGN);
 
     //Installing the signal handler for SIGTSSTP signal. Change to appropriate signal, if 
@@ -72,14 +99,9 @@ int main(int argc, char **argv) {
 
     CHANGE_FLAG =1; //Specify the change flag for server transmission behaviour.
 
-    operate_mode();
-       
-}
-
-
-void operate_mode(){
     
     while(1){
+
 
         STOP_FLAG = 0; //Reset the flag, this is a new role start!
 
@@ -128,7 +150,7 @@ void server_mode(){
         printf("The pointer to the buffer allocated is:%lld\n",(long long)buffer);
     #endif
 
-    //**********************************************************************************
+    //******************************************************************************************
 
      
     listenfd = Open_listenfd(port);
@@ -139,8 +161,7 @@ void server_mode(){
   
     
     while (1) {
-        if(STOP_FLAG)
-          break;
+        
         connfd = Malloc(sizeof(int));
         *connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
         int loc_connfd = *connfd;
@@ -203,6 +224,8 @@ void send_file(int clientfd) {
 
     Rio_readinitb(&clientRio, clientfd);
 
+    int resp_indicator;
+
     long long pointer;
     rio_readnb(&clientRio,&pointer,sizeof(long long));
 
@@ -221,7 +244,88 @@ void send_file(int clientfd) {
 
     buffer_type * send_buf_ptr = (buffer_type *)(pointer);
 
+
+//*********************************************SEND THE BUFFER******************************
+    // Send this buffer whenever change_flag changes, after every sent command wait for ACK. If NACK, switch to above code
+    //to receive a new buffer request and number of bytes request. All looping while blocks should also poll for the STOP_FLAG 
+    //to switch the roles. NOTE!!!! remove the STOP flag looping in previous calling function, and use it here.
+
+    
+
+    //Irrespective of change flag , send the buffer for one time anyway!
     rio_writen(clientfd,send_buf_ptr,size_req);
+
+    //Read the response from the client to see weather to continue/stop/break for role reversal!
+    rio_readnb(&clientRio,&resp_indicator,sizeof(int));
+
+
+
+
+
+//*****************************************************************************************************
+    if(resp_indicator==ACK){
+        //GOod to go continue with change flag based work!
+
+
+
+    }
+    else if(resp_indicator==NACK){
+        //Switch to new buffer request go to previous code block.
+
+    }
+    else{
+        //Role reversal! quit!
+
+
+    }
+
+
+
+
+//***********************************************GOOD TRY*************************************
+
+    while(1){
+
+    int current_value = CHANGE_FLAG;
+
+    while(1){
+
+        if((current_value)!=CHANGE_FLAG){ //Value of flag changed! send data once and wait for response!
+            //Time this as a constraint for change flag transition allowed times!
+            rio_writen(clientfd,send_buf_ptr,size_req);
+            rio_readnb(&clientRio,&resp_indicator,sizeof(int));
+            break;
+        }
+        //***************************************DEADLOCK POSSIBILITY HERE!***************************
+        //Find a way to break this loop here!!! timer based??!! counter based?? CREATE A NEW CONTROL SIGNAL???!!
+        //CLient here would be blocking on a read, and unless CHANGE_FLAG changes, server wont send anything->deadlock
+    }
+    if(resp_indicator==NACK){
+        break; //execute from new buffer read request block!
+    }
+    if(resp_indicator==STOP_MSG){
+        break; //exit and switch role!!! 
+
+
+    } 
+
+    }
+
+
+    //************************************************************************************************
+
+    
+
+    // while((current_value==CHANGE_FLAG)&&(STOP_FLAG==0));  //Wait till the CHANGE_FLAG changes!
+    
+    // if(STOP_FLAG==0)
+    // rio_writen(clientfd,send_buf_ptr,size_req);
+
+
+
+    // }
+
+    // rio_writen(clientfd,send_buf_ptr,size_req);
 
     #ifdef DEBUG_PRINT
         printf("    Server: Client received buffer\n");
@@ -241,10 +345,12 @@ void receive_file(long long *ptr, int num_bytes, char *host, char *port) {
     char buf[MAXBUF];
     rio_t remoteRio;
 
-    #ifdef DEBUG_PRINT
-        struct timeval start, finish, result;
-        gettimeofday(&start, NULL);
-    #endif
+    int resp_indicator;
+
+    // #ifdef DEBUG_PRINT
+    //     struct timeval start, finish, result;
+    //     gettimeofday(&start, NULL);
+    // #endif
 
     remotefd = Open_clientfd(host, port);
 
@@ -253,6 +359,7 @@ void receive_file(long long *ptr, int num_bytes, char *host, char *port) {
     //Request the buffer pointer.
     rio_writen(remotefd, ptr,sizeof(long long));
 
+    //Request the number of bytes.
     rio_writen(remotefd, &num_bytes,sizeof(int));
     
     //then get back if the file was found or not
@@ -261,6 +368,8 @@ void receive_file(long long *ptr, int num_bytes, char *host, char *port) {
     #ifdef DEBUG_PRINT
         printf("Response: %s", buf);
     #endif  
+
+    //**********************************CHECK!!!!!!!! FOR CLEANUP on unexpected exit here*****************
     
     if(!strcmp(buf,"error\n")) {//file not found
         printf("Client: Error getting file from server\n");
@@ -269,10 +378,40 @@ void receive_file(long long *ptr, int num_bytes, char *host, char *port) {
     }
     
 
-    buffer_type int_buffer_2[num_bytes/sizeof(buffer_type)];
-    rio_readnb(&remoteRio,int_buffer_2,num_bytes);
+    //*****************************************READ THE BUFFER********************************************
+    buffer_type * int_buffer_2 = (buffer_type *)malloc(num_bytes/sizeof(buffer_type));
 
+    
 
+    while(1){ //If Stop_flag is raised quit!
+
+        rio_readnb(&remoteRio,int_buffer_2,num_bytes);
+          
+        if(DISCONTINUE_FLAG==0){
+            //Send ACK  and continue!
+            resp_indicator = ACK;
+            rio_writen(remotefd,&resp_indicator,sizeof(int)); 
+          
+        }
+        else{
+            //Send NACK and break!
+            resp_indicator = NACK;
+            rio_writen(remotefd,&resp_indicator,sizeof(int)); 
+            break;  
+        }
+
+        if(STOP_FLAG){ //Send Stop message to switch roles! and break.
+            resp_indicator = STOP_MSG;
+            rio_writen(remotefd,&resp_indicator,sizeof(int));
+            break;
+        }
+    }
+    
+
+    //******************Send ACK to keep continuing receiving or send NACK to get a chance to ask for a new buffer->
+    //based on a continue flag. if continue flag =0 send ACK. if continue flag = 1 send NACK.
+
+    free(int_buffer_2);
 //***************************************************PRINT THE BUFFER****************************************
     #ifdef BUFFER_PRINT
     for(int i=0;i<(num_bytes/sizeof(buffer_type));i++){
@@ -287,21 +426,21 @@ void receive_file(long long *ptr, int num_bytes, char *host, char *port) {
 
     //print some stats about our transfer
 
-    #ifdef DEBUG_PRINT
-        gettimeofday(&finish, NULL);
-        timeval_subtract(&result, &finish, &start);
-        time_t elapsedSeconds = result.tv_sec;
-        long int elapsedMicros = result.tv_usec;
-        double elapsed = elapsedSeconds + ((double) elapsedMicros)/1000000.0;
+    // #ifdef DEBUG_PRINT
+    //     gettimeofday(&finish, NULL);
+    //     timeval_subtract(&result, &finish, &start);
+    //     time_t elapsedSeconds = result.tv_sec;
+    //     long int elapsedMicros = result.tv_usec;
+    //     double elapsed = elapsedSeconds + ((double) elapsedMicros)/1000000.0;
 
-        double fileSize = num_bytes*sizeof(int);
-        double speed = fileSize / elapsed;
+    //     double fileSize = num_bytes*sizeof(int);
+    //     double speed = fileSize / elapsed;
 
 
-        printf("Elapsed: %lf seconds\n", elapsed);
-        printf("Filesize: %0lf Bytes, %3lf kiloBytes, %lf megaBytes\n", fileSize, fileSize/1000, fileSize/1000000);
-        printf("Speed: %0lf B/s, %3lf kB/s, %lf mB/s\n", speed, speed/1000, speed/1000000);
-    #endif 
+    //     printf("Elapsed: %lf seconds\n", elapsed);
+    //     printf("Filesize: %0lf Bytes, %3lf kiloBytes, %lf megaBytes\n", fileSize, fileSize/1000, fileSize/1000000);
+    //     printf("Speed: %0lf B/s, %3lf kB/s, %lf mB/s\n", speed, speed/1000, speed/1000000);
+    // #endif 
     
 
 }
